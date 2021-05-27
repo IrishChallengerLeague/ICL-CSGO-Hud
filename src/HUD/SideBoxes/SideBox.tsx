b47ac53d2fc465b512e373c7d53882e5fa471784
Yannick Gloster <yannickgloster@gmail.com>
2021-05-27 21:54:16 +0100

    Customized sidebox image

--- src/HUD/SideBoxes/SideBox.tsx
+++ src/HUD/SideBoxes/SideBox.tsx
@@ -1,49 +1,73 @@
-import React from 'react';
-import './sideboxes.scss'
-import {configs, hudIdentity} from './../../App';
-import { apiUrl } from '../../api/api';
+import React from "react";
+import "./sideboxes.scss";
+import { configs, hudIdentity } from "./../../App";
+import { apiUrl } from "../../api/api";
 
-export default class SideBox extends React.Component<{ side: 'left' | 'right', hide: boolean}, { title: string, subtitle: string, image?: string }> {
-	constructor(props: any) {
-		super(props);
-		this.state = {
-            title:'Title',
-            subtitle:'Content',
-		}
-	}
+export default class SideBox extends React.Component<
+  { side: "left" | "right"; hide: boolean },
+  { title: string; subtitle: string; hide: boolean; image?: string }
+> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      title: "Title",
+      subtitle: "Content",
+      hide: this.props.hide,
+    };
+  }
 
-	componentDidMount() {
-        configs.onChange((data:any) => {
-            if(!data) return;
-            const display = data.display_settings;
-            if(!display) return;
-            if(`${this.props.side}_title` in display){
-                this.setState({title:display[`${this.props.side}_title`]})
-            }
-            if(`${this.props.side}_subtitle` in display){
-                this.setState({subtitle:display[`${this.props.side}_subtitle`]})
-            }
-            if(`${this.props.side}_image` in display){
-                const imageUrl = `${apiUrl}api/huds/${hudIdentity.name || 'dev'}/display_settings/${this.props.side}_image?isDev=${hudIdentity.isDev}&cache=${(new Date()).getTime()}`;
-                this.setState({image:imageUrl})
-            }
-        });
-	}
-	
-	render() {
-        const { image, title, subtitle} = this.state;
-        if(!title) return '';
-		return (
-			<div className={`sidebox ${this.props.side} ${this.props.hide ? 'hide':''}`}>
-                <div className="title_container">
-                    <div className="title">{title}</div>
-                    <div className="subtitle">{subtitle}</div>
-                </div>
-                <div className="image_container">
-                    {image ? <img src={image} id={`image_left`} alt={'Left'}/>:''}
-                </div>
-            </div>
-		);
-	}
+  componentDidMount() {
+    configs.onChange((data: any) => {
+      if (!data) return;
+      const display = data.display_settings;
+      if (!display) return;
+      if (`${this.props.side}_title` in display) {
+        this.setState({ title: display[`${this.props.side}_title`] });
+      }
+      if (`${this.props.side}_subtitle` in display) {
+        this.setState({ subtitle: display[`${this.props.side}_subtitle`] });
+      }
+      if (`${this.props.side}_image` in display) {
+        const imageUrl = `${apiUrl}api/huds/${
+          hudIdentity.name || "dev"
+        }/display_settings/${this.props.side}_image?isDev=${
+          hudIdentity.isDev
+        }&cache=${new Date().getTime()}`;
+        const imageTest = new XMLHttpRequest();
+        imageTest.open("HEAD", imageUrl, false);
+        imageTest.send();
+        if (imageTest.status !== 404) {
+          this.setState({ image: imageUrl });
+        }
+      }
+      if (
+        display[`${this.props.side}_title`] === "" &&
+        display[`${this.props.side}_subtitle`] === "" &&
+        display[`${this.props.side}_image`] === ""
+      ) {
+        this.setState({ hide: true });
+      } else {
+        this.setState({ hide: false });
+      }
+    });
+  }
 
+  render() {
+    const { image, title, subtitle, hide } = this.state;
+    return (
+      <div className={`sidebox ${this.props.side} ${hide ? "hide" : ""}`}>
+        {title ? (
+          <div className="title_container">
+            <div className="title">{title}</div>
+            <div className="subtitle">{subtitle ? subtitle : ""}</div>
+          </div>
+        ) : (
+          ""
+        )}
+        <div className={title ? "image_container" : "image_container_full"}>
+          {image ? <img src={image} id={`image_left`} alt={"Left"} /> : ""}
+        </div>
+      </div>
+    );
+  }
 }
